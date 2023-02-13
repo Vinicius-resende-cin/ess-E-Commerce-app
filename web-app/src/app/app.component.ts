3a79f741dd1964ce1385835febaeb58e61299c1d
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-02-12 23:52:42 -0300

    Definição do estado inicial do projeto (front e backend base para início do desenvolvimento)

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import * as Names from './common/global-names';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'web-app';
+  title = Names.appTitle;
 }
