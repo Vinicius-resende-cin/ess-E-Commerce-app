2441718a0ad1c8656f23cab87ea9fa133cfda396
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-03-27 19:40:23 -0300

    [SYSTEM] restrição de acesso da página de usuário logado apenas para sessões ativas

---
+++ web-app/src/app/modules/main-page-routing/main-page-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainPageRoutingRoutingModule } from './main-page-routing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MainPageRoutingRoutingModule
+  ]
+})
+export class MainPageRoutingModule { }
