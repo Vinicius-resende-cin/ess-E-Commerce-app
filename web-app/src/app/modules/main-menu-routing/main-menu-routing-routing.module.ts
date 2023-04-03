894fa40c3c3f1ed44ef4dc7d21d661f18cda069f
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2023-04-03 20:17:16 -0300

    [SYSTEM] Realocação de rotas para a página de usuário logado

--- web-app/src/app/modules/main-menu-routing/main-menu-routing-routing.module.ts
+++ web-app/src/app/modules/main-menu-routing/main-menu-routing-routing.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { authGuardMain } from 'src/app/common/auth.guard';
+import { authGuardLogin } from 'src/app/common/auth.guard';
 import { GeneralMainPageComponent } from 'src/app/components/general-main-page/general-main-page.component';
 import { MainMenuComponent } from 'src/app/components/main-menu/main-menu.component';
-import { ProductDetailComponent } from 'src/app/components/product-detail/product-detail.component';
-import { ViewHistoryComponent } from 'src/app/components/view-history/view-history.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainMenuComponent,
-    canActivate: [authGuardMain],
-    children: [
-      { path: '', component: GeneralMainPageComponent },
-      { path: 'historico-pedidos', component: ViewHistoryComponent },
-      { path: 'item/:id', component: ProductDetailComponent },
-    ],
+    canActivate: [authGuardLogin],
+    children: [{ path: '', component: GeneralMainPageComponent }],
   },
 ];
 
