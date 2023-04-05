e4054d3883679637b25d7dc24bf7b4acd5a20be9
erlanl <erlanlirajr@gmail.com>
2023-04-05 15:08:06 -0300

    fix: troca id manual pelo id do mongodb

--- server/api/controllers/itens.ts
+++ server/api/controllers/itens.ts
@@ -5,8 +5,9 @@ module.exports = () => {
       listProducts: async (req: any, res: any) => {
         /** Retorna todos o item */
         try {
-          const id_produto: string = <string> req.params.id;
-          const itens = await Item.findOne({id: id_produto}, { _id: false });
+          //const id_produto: string = <string> req.params.id;
+          const id_mongo = require('mongodb').ObjectId;
+          const itens = await Item.findOne({id: id_mongo}, { _id: true });
           res.json(itens);
         } catch (err) {
           res.status(500).send(err);
@@ -20,7 +21,7 @@ module.exports = () => {
             let faltaInfo = false;
 
             Object.keys(req.body).forEach((item) => {
-              if (!req.body[item] || req.body[item] === ""){
+              if ((!req.body[item] || req.body[item] === "") && req.body[item] != req.body._id){
                 faltaInfo = true;
               }
             });
@@ -32,7 +33,6 @@ module.exports = () => {
               res.status(400).json({ message: "Já existe um Item com esse titulo" });
 
             } else {
-              console.log('Dados enviados!')
               const newItem = await Item.create(req.body);
               res.status(200).json(newItem);
 
