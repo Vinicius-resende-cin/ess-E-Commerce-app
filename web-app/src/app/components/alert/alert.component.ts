4fca8b14c1790da1b6a8ebb799bc1faad6dddf19
Felipe Moraes <felipe3902@gmail.com>
2023-04-07 01:18:15 -0300

    [Criacao Categorias] Removida dependência de Alert

--- web-app/src/app/components/alert/alert.component.ts
+++ web-app/src/app/components/alert/alert.component.ts
@@ -3,6 +3,8 @@ import { Component, Input } from '@angular/core';
 import { CategoryCreationComponent } from '../category-creation/category-creation.component';
 import { CategoryUpdateComponent } from '../category-update/category-update.component';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
@@ -12,29 +14,14 @@ export class AlertComponent {
   @Input() message: string;
   show = true;
 
-  /*
-  constructor(private categoryCreation: CategoryCreationComponent) {
-    this.message = "";
-  }
-
-    close(): void {
-    this.show = false;
-    this.categoryCreation.clearErros();
-    this.categoryCreation.sairDaCriacao();
-  }
-  */
-
-
-  constructor(private categoryUpdate: CategoryUpdateComponent) {
-    this.message = "";
+  constructor(private router: Router) {
+      this.message = "";
   }
 
   close(): void {
     this.show = false;
-    this.categoryUpdate.clearErros();
-    this.categoryUpdate.sairDaCriacao();
+    this.router.navigate(['home', 'categoria']);
   }
-
 
   get_show(): boolean {
     return this.show;
