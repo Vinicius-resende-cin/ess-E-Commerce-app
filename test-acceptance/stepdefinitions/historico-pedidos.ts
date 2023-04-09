a279cb50ed0aad1403cf62322ce677cbd22f94ce
Amanda-Quirino <aqrs@cin.ufpe.br>
2023-04-09 19:08:13 -0300

    Refactor: Refatorando o stepdefinitions do histórico de compras

--- test-acceptance/stepdefinitions/historico-pedidos.ts
+++ test-acceptance/stepdefinitions/historico-pedidos.ts
@@ -21,6 +21,11 @@ const formatDateToInput = (dt: string): string => {
     return split_dt[1] + "-" + split_dt[0];
 };
 
+function filtrarPorId(str:string, elemento: ElementArrayFinder) {
+    return elemento.filter(elem => (elem.getAttribute('id')
+    .then(ids => ids === str)));
+}
+
 defineSupportCode(function({Given, When, Then}) {
     Given(/^Estou vendo o histórico de pedidos da minha conta$/, async() => {
         await browser.get(baseUrl + "home/historico-pedidos");
@@ -29,15 +34,15 @@ defineSupportCode(function({Given, When, Then}) {
     Given(/^Estou vendo o item "(.*)" do pedido "(\d*)" no histórico$/, async(item, pedido) => {
         await browser.sleep(3000);
         var allItens = element.all(by.name('item'));
-        await allItens.filter(elemento => (elemento.getAttribute('id').then(ids => ids === 'btn-' + pedido + '-' + item))).then
-            (elemento => expect(Promise.resolve(elemento.length)).to.eventually.equal(1));
+
+        await filtrarPorId('btn-' + pedido + '-' + item, allItens).then
+        (elemento => expect(Promise.resolve(elemento.length)).to.eventually.equal(1));
     });
 
     Given(/^Vejo que o produto "(.*)" do pedido "(\d*)" ainda tem "(\d*)" em estoque no sistema$/, async(item, pedido, quant) => {
         var allQuantidade = element.all(by.name('estoque'));
 
-        var elem = (allQuantidade.filter(elemento => (elemento.getAttribute('id').then
-        (ids => ids === 'estoque-' + pedido + '-' + item)))).first();
+        var elem = filtrarPorId('estoque-' + pedido + '-' + item, allQuantidade).first();
 
         await expect(Promise.resolve(elem.getText())).to.eventually.equal(quant);
     });
@@ -45,8 +50,7 @@ defineSupportCode(function({Given, When, Then}) {
     Given(/^Vejo que o produto "(.*)" do pedido "(\d*)" não está em estoque no sistema$/, async(item, pedido) => {
         var allQuantidade = element.all(by.name('estoque'));
 
-        var elem = (allQuantidade.filter(elemento => (elemento.getAttribute('id').then
-        (ids => ids === 'estoque-' + pedido + '-' + item)))).first();
+        var elem = filtrarPorId('estoque-' + pedido + '-' + item, allQuantidade).first();
 
         await expect(Promise.resolve(elem.getText())).to.eventually.equal('0');
     });
@@ -54,8 +58,7 @@ defineSupportCode(function({Given, When, Then}) {
     Given(/^foram compradas "(\d*)" "(.*)" no pedido "(\d*)"$/, async(quant, item, pedido) => {
         var allQuantidade = element.all(by.name('quantidade'));
 
-        var elem = (allQuantidade.filter(elemento => (elemento.getAttribute('id').then
-        (ids => ids === 'quantidade-' + pedido + '-' + item)))).first();
+        var elem = filtrarPorId('quantidade-' + pedido + '-' + item, allQuantidade).first();
 
         await expect(Promise.resolve(elem.getText())).to.eventually.equal(quant);
     });
@@ -73,8 +76,7 @@ defineSupportCode(function({Given, When, Then}) {
     When(/^Eu seleciono o produto "(.*)" do pedido "(\d*)"$/, async(item, pedido) => {
         var allItens = element.all(by.name('item'));
 
-        var elem = (allItens.filter(elemento => (elemento.getAttribute('id').then
-        (ids => ids === 'btn-' + pedido + '-' + item)))).first();
+        var elem = filtrarPorId('btn-' + pedido + '-' + item, allItens).first();
 
         await elem.click();
     });
@@ -82,8 +84,7 @@ defineSupportCode(function({Given, When, Then}) {
     When (/^Eu seleciono "(.*)" no pedido "(\d*)"$/, async(nome, pedido) => {
         var allComprar = element.all(by.name('comprar'));
 
-        var elem = (allComprar.filter(elemento => (elemento.getAttribute('id').then
-        (ids => ids === 'btn-' + pedido)))).first();
+        var elem = filtrarPorId('btn-' + pedido, allComprar).first();
 
         await elem.click();
     });
@@ -116,22 +117,21 @@ defineSupportCode(function({Given, When, Then}) {
     Then(/^Eu vejo que o produto "(.*)" esta no carrinho de compras$/, async(item) => {
         var allItens = element.all(by.name('item'));
 
-        await allItens.filter(elemento => (elemento.getAttribute('id').then(ids => ids === 'nome-' + item))).then
+        await filtrarPorId('nome-' + item, allItens).then
             (elemento => expect(Promise.resolve(elemento.length)).to.eventually.equal(1));
     });
 
     Then(/^Eu vejo que o produto "(.*)" não esta no carrinho de compras$/, async(item) => {
         var allItens = element.all(by.name('item'));
 
-        await allItens.filter(elemento => (elemento.getAttribute('id').then(ids => ids === 'nome-' + item))).then
-            (elemento => expect(Promise.resolve(elemento.length)).to.eventually.equal(0));
+        await filtrarPorId('nome-' + item, allItens).then
+            (elemento => expect(Promise.resolve(elemento.length)).to.eventually.equal(0))
     });
 
     Then(/^Eu vejo que "(\d*)" unidades do item "(.*)" estão no carrinho de compras$/, async(quant, item) => {
         var allItens = element.all(by.name('quant'));
 
-        var elem = (allItens.filter(elemento => (elemento.getAttribute('id').then
-        (ids => ids === 'quant-' + item)))).first();
+        var elem = filtrarPorId('quant-' + item, allItens).first();
 
         await expect(Promise.resolve(elem.getText())).to.eventually.equal(quant);
     });
