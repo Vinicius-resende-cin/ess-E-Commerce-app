c34fb3d935502bf62212de15e61f449eb1f6b681
pedroalfonseca <paalf1337@gmail.com>
2023-04-10 05:58:54 -0300

    fix: ajustes no frontend da feature 'gerar-link'

--- web-app/src/app/components/generate-link/generate-link.component.ts
+++ web-app/src/app/components/generate-link/generate-link.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CustomLinksService } from 'src/app/services/customLinks.service';
+
+import { NavigationExtras, NavigationEnd, Router } from '@angular/router';
+import { ResetPasswordComponent } from '../reset-password/reset-password.component';
 
 @Component({
   selector: 'app-generate-link',
@@ -6,7 +10,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./generate-link.component.scss']
 })
 export class GenerateLinkComponent {
-  constructor() {}
+  constructor(
+    private customLinksService: CustomLinksService,
+    private router: Router,
+  ) {}
 
   ngOnInit() {}
 
@@ -14,18 +21,39 @@ export class GenerateLinkComponent {
     window.history.back();
   }
 
-  copyToClipboard(inputElement: HTMLInputElement) {
-    inputElement.select();
+  private hash(str: string) {
+    var hash = 5381;
 
-    if (inputElement.value.length == 0) {
-      alert('Erro: o nome do link não pode ser vazio.');
-    } else if (inputElement.value.length < 8) {
+    for (var i = 0; i < str.length; i++) {
+      hash = (hash * 33) ^ str.charCodeAt(i);
+    }
+
+    return hash >>> 0;
+  }
+
+  copyToClipboard(input1: HTMLInputElement, input2: HTMLInputElement) {
+    input1.select();
+    input2.select();
+
+    if (input2.value.length == 0) {
+      alert('Erro: O nome do link não pode ser vazio.');
+    } else if (input2.value.length < 8) {
       alert('Erro: O nome do link deve ter 8 caracteres no mínimo.');
-    } else if (inputElement.value.length > 15) {
+    } else if (input2.value.length > 15) {
       alert('Erro: O nome do link pode ter 15 caracteres no máximo.');
     } else {
-      navigator.clipboard.writeText(inputElement.value);
-      alert('Link copiado para a área de transferência.');
+      const id: number = this.hash(input2.value);
+      const url: string = `http://commerCin-${input2.value}.${input1.value.slice(7)}`;
+
+      this.customLinksService.generateCustomLink(id, url).subscribe(
+        (response) => {
+          navigator.clipboard.writeText(url);
+          alert('Sucesso: Link copiado para a área de transferência.');
+        },
+        (error) => {
+          alert('Erro: Não foi possível gerar o link.');
+        }
+      );
     }
   }
 }
